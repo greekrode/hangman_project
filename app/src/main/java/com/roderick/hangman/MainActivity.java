b97ec2bd28bd779522587d7b0850e96d350a42b1
Dixon Verrix <dixon.verrix13@gmail.com>
2017-07-10 17:35:27 +0700

    added mainActivity interaction with new game button (resume and setting not yet)

--- app/src/main/java/com/roderick/hangman/MainActivity.java
+++ app/src/main/java/com/roderick/hangman/MainActivity.java
@@ -1,7 +1,23 @@
 package com.roderick.hangman;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonNew = (Button) findViewById(R.id.newgame);
+        buttonNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), PlayGame.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
