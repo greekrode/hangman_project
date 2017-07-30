95cc0509c52c23dacda2ba24abc22d1dc681da5c
Dixon Verrix <dixon.verrix13@gmail.com>
2017-07-30 19:55:48 +0700

    Edited end game fragment

--- app/src/main/java/com/roderick/hangman/EndGameFragment.java
+++ app/src/main/java/com/roderick/hangman/EndGameFragment.java
@@ -1,29 +1,21 @@
 package com.roderick.hangman;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EndGameFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EndGameFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EndGameFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -33,14 +25,6 @@ public class EndGameFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EndGameFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static EndGameFragment newInstance(String param1, String param2) {
         EndGameFragment fragment = new EndGameFragment();
@@ -63,8 +47,42 @@ public class EndGameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.retry_game,container,false);
+        TextView status = (TextView)view.findViewById(R.id.textView2);
+        //ganti value dari textview
+        TextView score = (TextView) view.findViewById(R.id.score);
+        //update score ke textview
+        String strtext = getArguments().getString("Status");
+        if(strtext.equals("Win")){
+            String times = getArguments().getString("Chance");
+            Integer value1 = Integer.parseInt(times);
+            value1 = value1 * 100;
+            status.setText("You Win!");
+            score.setText(value1.toString());
+        }else{
+            status.setText("You Lose!");
+            score.setText("0");
+        }
+        Button resetButton = (Button)view.findViewById(R.id.retry);
+        Button mainMenuButton = (Button)view.findViewById(R.id.back);
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PlayGame.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_end_game, container, false);
+        return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -73,34 +91,12 @@ public class EndGameFragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
