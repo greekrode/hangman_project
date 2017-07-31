0526a3979b05a28f0da2f64a64d40df44b508e06
OSSAS\roder <roderickhalim@gmail.com>
2017-07-31 20:43:02 +0700

    added show high score activity

---
+++ app/src/main/java/com/roderick/hangman/realm/HighScore.java
@@ -0,0 +1,32 @@
+package com.roderick.hangman.realm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roderick.hangman.R;
+import com.roderick.hangman.model.Score;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+/**
+ * Created by roder on 7/31/2017.
+ */
+
+public class HighScore extends AppCompatActivity {
+    public Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.high_score);
+    }
+
+    public void viewScore(){
+        RealmResults<Score> results = realm.where(Score.class).findAll();
+
+        for (Score score : results){
+            //text.append(score.getName() + " " + score.getScore() + "\n");
+        }
+    }
+}
