1cd5d13da970ac2cf76fcd101216b8949eef0d59
Dixon Verrix <dixon.verrix13@gmail.com>
2017-08-09 16:43:24 +0700

    Edited settings to determine difficulty

--- app/src/main/java/com/roderick/hangman/Settings.java
+++ app/src/main/java/com/roderick/hangman/Settings.java
@@ -3,7 +3,10 @@ package com.roderick.hangman;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by roder on 8/1/2017.
@@ -17,25 +20,32 @@ public class Settings extends AppCompatActivity {
     }
 
     public void changeDifficulty(){
-        RadioButton button_easy = (RadioButton)findViewById(R.id.easy);
-        RadioButton button_medium = (RadioButton)findViewById(R.id.medium);
-        RadioButton button_hard = (RadioButton)findViewById(R.id.hard);
-
-        if (button_easy.isChecked()){
-            Intent intent = new Intent(getBaseContext(), PlayGame.class);
-            String difficulty = "easy";
-            intent.putExtra("Difficulty",difficulty);
-            startActivity(intent);
-        } else if (button_medium.isChecked()){
-            Intent intent = new Intent(getBaseContext(), PlayGame.class);
-            String difficulty = "medium";
-            intent.putExtra("Difficulty",difficulty);
-            startActivity(intent);
-        } else if (button_hard.isChecked()){
-            Intent intent = new Intent(getBaseContext(), PlayGame.class);
-            String difficulty = "hard";
-            intent.putExtra("Difficulty",difficulty);
-            startActivity(intent);
-        }
+        Button back = (Button) findViewById(R.id.button3);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        // Inflate the layout for this fragment
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+                if(checkedId == R.id.radioButtonEasy){
+                    PlayGame.Difficulty = "Easy";
+                }else if(checkedId==R.id.radioButtonNormal){
+                    PlayGame.Difficulty = "Normal";
+                }else if(checkedId==R.id.radioButtonHard){
+                    PlayGame.Difficulty = "Hard";
+                }
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+//                getActivity().getFragmentManager().popBackStack("e",
+//                        FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                //  getActivity().getSupportFragmentManager().popBackStackImmediate("Frag1",0);
+            }
+        });
     }
 }
